56c5018fd91086e89fa0690bff65659e0b419fe6
wangybnet <wangyb.net@hotmail.com>
2016-03-05 20:59:25 +0800

    refactored development directory

--- kafka/testOperation.py
+++
@@ -1,7 +0,0 @@
-from Operation import Operation
-import unittest
-class testOperation(unittest.TestCase):
-    def testProduct(self):
-        op = Operation()
-        self.assertEqual(op.product(2,3),6)
-        self.assertEqual(op.product(0,7),0)
