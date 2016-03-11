9a4473e7ce821d3b6622f9fd3e82dc666ceaee69
ChiaoChiong <chiao@zhaochang.org>
2016-03-11 16:52:06 +0800

    start函数改变

--- tests/test_master/master_test.py
+++ tests/test_master/master_test.py
@@ -14,15 +14,17 @@ class TestMaster(unittest.TestCase):
         self.task = {"test" : "test_create"}
         print("开始测试")
 
-    def test_create(self):
+    def test_create_one(self):
         master = Master()
         global inserted_id
-        inserted_id = master.create(self.task)
+        inserted_id = master.create_one(self.task)
         self.assertIsNotNone(inserted_id)
 
-    def test_test(self):
+    def test_start_one(self):
         master = Master()
-        master.test()
+        global inserted_id
+        master.start_one(inserted_id)
+
 
     @classmethod
     def tearDownClass(self):
@@ -30,7 +32,7 @@ class TestMaster(unittest.TestCase):
         global inserted_id
         global flag
         print(inserted_id)
-        flag = master.delete(inserted_id)
+        flag = master.delete_one(inserted_id)
         print("测试结束")
 
 if __name__ == '__main__':
--- wsp/master/master.py
+++ wsp/master/master.py
@@ -2,10 +2,14 @@
 
 
 from pymongo import MongoClient
+from wsp.fetcher.fetcherManager import fetcherManager
 
 
 
 class Master(object):
+
+    def __init__(self):
+        self.fetcher_manager = fetcherManager()
 
     #建立mongodb连接并选择集合
     def __get_col(self, db_name, col_name):
@@ -26,7 +30,16 @@ class Master(object):
     def start_one(self, task_id):
         collection = self.__get_col('wsp', 'task')
         task = collection.find_one({'_id' : task_id})
-        return task
+        tasks = []
+        tasks.append(task)
+        flag = self.fetcher_manager.start(tasks)
+        if flag:
+            print('任务已经启动')
+        else:
+            print('任务启动失败')
+
+
+
 
     def stop_one(self, task_id):
         collection = self.__get_col('wsp', 'task')
