0366acc877bc7b98ee891966e9f9e3863f94e2ff
ChiaoChiong <chiao@zhaochang.org>
2016-03-11 16:07:06 +0800

    master.py

--- wsp/master/master.py
+++ wsp/master/master.py
@@ -13,19 +13,25 @@ class Master(object):
         collection = client[db_name][col_name]
         return collection
 
-    def create(self, task):
+    def create_one(self, task):
         collection = self.__get_col('wsp', 'task')
         task_id = collection.insert_one(task).inserted_id #返回任务ID
         return task_id
 
-    def delete(self, task_id):
+    def delete_one(self, task_id):
         collection = self.__get_col('wsp', 'task')
         flag = collection.remove({'_id' : task_id})
         return flag
 
+    def start_one(self, task_id):
+        collection = self.__get_col('wsp', 'task')
+        task = collection.find_one({'_id' : task_id})
+        return task
 
-
-
+    def stop_one(self, task_id):
+        collection = self.__get_col('wsp', 'task')
+        task = collection.find_one({'_id' : task_id})
+        return task
 
 
 
