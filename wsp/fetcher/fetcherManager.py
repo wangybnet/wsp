366d3a444ce70f210bcb8c7384bb34797092d8a5
Gladys Lau <390653166@qq.com>
2016-03-16 11:09:41 +0800

    修改接口2

--- wsp/fetcher/fetcherManager.py
+++ wsp/fetcher/fetcherManager.py
@@ -18,43 +18,39 @@ class fetcherManager:
         db = conn.wsp
         self.taskTable = db.tasks
 
-    def start(self):
+    def _start(self):
         for f in self.fetcherList:
             rpcClient = ServerProxy(f)
             rpcClient.changeTasks(self.running_tasks)
         # TODO:默认返回True 之后可能根据rpc连接情况修改
         return True
 
-    def create(self,tasks):
-        for t in tasks:
-            self.taskTable.update({"id":t.id},{"$set":{'status':1}})
-            self.running_tasks.append(t)
-            for url in t.start_urls:
-                req = WspRequest()
-                req.id = ObjectId()
-                req.father_id = req.id
-                req.task_id = t.id
-                self.pushReq(req)
-            self.producer.flush()
-        return self.start()
-
     def delete(self,tasks):
+        # TODO: 从kafka中删除任务
         for t in tasks:
             self.taskTable.update({"id":t.id},{"$set":{'status':3}})
             self.runing_tasks.remove(t)
-        return self.start()
+        return self._start()
 
     def stop(self,tasks):
         for t in tasks:
             self.taskTable.update({"id":t.id},{"$set":{'status':2}})
             self.runing_tasks.remove(t)
-        self.start()
+        return self._start()
 
-    def restart(self,tasks):
+    def start(self,tasks):
         for t in tasks:
+            if t.status==0:
+                for url in t.start_urls:
+                    req = WspRequest()
+                    req.id = ObjectId()
+                    req.father_id = req.id
+                    req.task_id = t.id
+                    self.pushReq(req)
+                self.producer.flush()
             self.taskTable.update({"id":t.id},{"$set":{'status':1}})
             self.running_tasks.append(t)
-        return self.start()
+        return self._start()
 
 
 
